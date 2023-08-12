781c32656ad1520d867cfc2876e28598fc748d7d
Kawa Tai 119 <58183232+kawatainhat02@users.noreply.github.com>
2023-08-13 06:13:57 +0900

    Create .cpp

---
+++ .cpp
@@ -0,0 +1,78 @@
+class Rectangle {
+    int width, height;
+  public:
+    void set_values (int,int);
+    int area (void);
+} rect;
+
+# include "BST_Node.h"
+
+BST_Node:: BST_Node()
+{
+	left = nullptr;
+	right = nullptr;
+	name = "";
+    adress = "";
+    account_number = 0;
+    password = 0;
+    balance = 0;
+}
+BST_Node:: BST_Node(string name, string adress, int accountno, int password, int balance)
+{
+	left = nullptr;
+	right = nullptr;
+	this->name = name;
+	this->account_number = accountno;
+	this->adress = adress;
+	this->balance = balance;
+	this->password = password;
+}
+
+# include "Hashtable.h"
+# include "BST_Tree.h"
+# include "admin.h"
+# include "staff.h"
+# include "customer.h"
+# include "string.h"
+
+void boot()
+{
+	Hashtable H;
+	BST_Tree T;
+	H.starthash();
+	T.load_Server();
+}
+int main()
+{
+	void boot();
+
+	int condition=0;
+	while (condition != 4)
+	{
+		cout << "YOU want to login as:\t\t\t\t\t\t" << endl << endl;
+		cout << "1-ADMIN" << endl;
+		cout << "2-STAFF" << endl;
+		cout << "3-CUSTOMER" << endl;
+		cin >> condition;
+		if (condition == 1)
+		{
+			admin();
+		}
+		if (condition == 2)
+		{
+
+		}
+		if (condition == 3)
+		{
+			customer();
+		}
+		if (condition == 4)
+		{
+			condition = 4;
+		}
+	}
+
+	
+	system("pause");
+	return 0;
+}
